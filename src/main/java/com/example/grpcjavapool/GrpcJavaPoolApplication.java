00178b0cf6633aa457562b6ff78eabd5fd8c9d37
weijian <weijian@baidu.com>
2022-05-12 15:05:13 +0800

    Initial commit

---
+++ src/main/java/com/example/grpcjavapool/GrpcJavaPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcjavapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJavaPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcJavaPoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grpcjavapool/GrpcJavaPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcjavapool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcJavaPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
